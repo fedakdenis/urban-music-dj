5a2ee7629185ace02e466600fdea9b9d5c26c09c
fedakdenis <fedakdenis@gmail.com>
2022-07-01 21:59:52 +0500

    Update views.py
    Test changes

--- mainloop/views.py
+++ mainloop/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
   
 def index(request):
-    return HttpResponse("<h2>Главная</h2>")
+    return HttpResponse("<h2>Главная</h2><h3>Новый текст</h3>")
  
 def about(request):
     return HttpResponse("<h2>О сайте</h2>")
