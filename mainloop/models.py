42987e5e8258b9bf21292f775a8641af46348994
fedakdenis <fedakdenis@gmail.com>
2022-07-08 19:20:12 +0500

    Camera model

--- mainloop/models.py
+++ mainloop/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Camera(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    group = models.CharField(max_length=255)
+    uid = models.UUIDField(default=uuid.uuid4, unique=True)
+    url = models.URLField()
+    description = models.TextField(null=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
+
